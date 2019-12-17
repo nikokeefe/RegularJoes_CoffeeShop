2c10f05cf4a5a996e22c99f1bce00aec9316a200
Nik O'Keefe <nikwillok@gmail.com>
2019-12-17 22:28:52 +1300

    added navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,10 +8,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import Navbar from "../components/Globals/Navbar"
+
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import { FaGulp } from "react-icons/fa"
 
